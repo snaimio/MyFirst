4826f67929867ae9c50e11cd0cca728c8757f1ec
Sheikh Naim <naimbgd@gmail.com>
2026-05-19 21:13:53 -0400

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Sheikh Naim on 2026-05-19.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
